62a399b714fa6438a13741d93277922000e79e30
test-aws <adyuri1257@gmail.com>
2021-05-27 13:41:44 +0900

    bu

---
+++ scripts/application_start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Stop all servers and start the server as a daemon
+cd /home/ec2-user/codedeploy-test-aws-web
+# install and use node version
+nvm install 14.4.0
+nvm use 14.4.0
+
+# print node and npm version
+node --version
+npm --version
+
+# upgrade npm and install forever
+npm install -g npm@latest
+npm install -g forever
